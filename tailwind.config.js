81f2f9521d84b1106d37db7fd1d9820fed576c3a
Richard Ziegler <37094831+richardziegler@users.noreply.github.com>
2022-06-08 18:35:29 -0500

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,njk}"],
   theme: {
-    extend: {},
+    maxWidth: {
+      '1/5': '20%'
+    },
+    extend: {
+      fontFamily: {
+        'press-start': ['"Press Start 2P"', 'cursive']
+      },
+    },
   },
   plugins: [],
 };
